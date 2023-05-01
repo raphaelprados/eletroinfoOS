0eb317de73addcd56bcf19960796470c18d94b93
raphaelprados <santosapraphael@gmail.com>
2023-05-01 10:23:52 -0400

    Base para desenvolvimento da pagina de cliente

---
+++ src/components/ClienteForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <form class="container-fluid">
+        <div class="d-flex flex-row">
+            <div class="d-flex flex-row">
+                <input type="text" class="form-control me-2" placeholder="Nome Completo" aria-label="Nome">
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'ClienteForm',
+    data() {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/ClienteView.vue
+++ src/views/ClienteView.vue
@@ -1,5 +1,8 @@
 <template>
-    <button type="button" class="btn btn-primary">Novo Cliente</button>
+    <button type="button" class="btn btn-primary" @click="toggleInserirCliente">Novo Cliente</button>
+    <div class="input-cliente" v-if="showClienteForm">
+        <ClienteForm/>
+    </div>
     <div class="tabela-clientes">
         <ul class="list-group">
             <div class="d-flex flex-row">
@@ -12,8 +15,36 @@
 </template>
 
 <script>
-export default {
+import ClienteForm from '@/components/ClienteForm.vue' 
 
+export default {
+    data() {
+        return {
+            showClienteForm: false,
+            cliente: {
+                nome: "",
+                telefone: "",
+                cpf: ""
+            },
+            endereco: {
+                estado: "",
+                cidade: "",
+                bairro: "",
+                rua: "",
+                numero: "",
+                tipo: "",
+                complemento: ""
+            }
+        }
+    },
+    methods: {
+        toggleInserirCliente() {
+            this.showClienteForm = !this.showClienteForm
+        }
+    }, 
+    components: {
+        ClienteForm
+    }
 }
 </script>
 
