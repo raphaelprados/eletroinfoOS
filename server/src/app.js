605864c19141248ccdbdb1e29c96c2dcba4000d5
raphaelprados <santosapraphael@gmail.com>
2023-05-09 19:26:15 -0400

    localhost:8081 funcionando com o básico

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,20 @@
 
-console.log('_ok_');
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'hello world bitch'
+    })
+})
+
+app.listen(8081, () => {
+    console.log('Server is running on port 8081')
+})
