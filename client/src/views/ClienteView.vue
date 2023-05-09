1c53557f7fbcb680899ae59c7ef89eac89c06b19
raphaelprados <santosapraphael@gmail.com>
2023-05-09 16:58:20 -0400

    Estrutura do projeto modificada

---
+++ client/src/views/ClienteView.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="search-header">
+        <div class="input-group" style="padding: 10px">
+            <span class="input-group-text" id="buscaLabel">Pesquisar</span>
+            <input class="form-control" type="search" id="buscaId" aria-label="Busca" 
+                    placeholder="Digite as tags de busca" aria-describedby="buscaLabel">
+        </div>
+        <button type="button" class="btn btn-primary" @click="toggleInserirCliente">Novo Cliente</button>
+    </div>
+    <Modal @close="toggleInserirCliente" v-show="showClienteForm">
+        <ClienteForm />
+    </Modal>    
+    <Listagem/>
+</template>
+
+<script>
+import ClienteForm from '@/components/ClienteForm.vue' 
+import Modal from '@/components/Modal.vue'
+import Listagem from '@/components/Listagem.vue'
+
+export default {
+    data() {
+        return {
+            showClienteForm: false,
+            cliente: {
+                nome: "",
+                telefone: "",
+                cpf: ""
+            },
+            endereco: {
+                estado: "",
+                cidade: "",
+                bairro: "",
+                rua: "",
+                numero: "",
+                tipo: "",
+                complemento: ""
+            }
+        }
+    }, 
+    components: {
+        ClienteForm,
+        Modal,
+        Listagem
+    },
+    methods: {
+        toggleInserirCliente() {
+            this.showClienteForm = !this.showClienteForm
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
