167e0371e8d40128daa7e55ba86b576b77c1b45c
raphaelprados <santosapraphael@gmail.com>
2023-12-22 17:42:53 -0400

    Conexao com o banco de dados estabelecida

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 import morgan from 'morgan'
 import pg from 'pg'
 
-import routes from './routes.js'
+// import routes from './routes.js'
 
 const app = express()
 app.use(morgan('combined'))
@@ -21,14 +21,20 @@ const client = new pg.Client({
     password: 'admin'
 })
 
-routes(app)
+// routes(app)
+
+app.post('/register', (req, res) => {
+    res.send({
+        message: `Hello ${req.body.email}! Your user was registered! Have fun!`
+    })
+})
 
 // Testando conexão do banco de dados
 client.connect((err) => {
     if(err)
         console.log('connection error', err.stack)
     else
-        console.log('connected');
+        console.log('connected to ' + client.database);
 })
 
 app.listen(8081, () => {
